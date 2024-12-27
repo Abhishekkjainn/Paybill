d2be8ad9057ef6ca0f13be9bcd14d9f750fa662b
abhishekkjainn <jainabhishek1904@gmail.com>
2024-12-27 17:28:24 +0530

    Added banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header';
 import Page1 from './pages/page1';
+import Page2 from './pages/page2';
 
 function App() {
   return (
     <>
       <Header />
       <Page1 />
+      <Page2 />
     </>
   );
 }
---
+++ src/pages/page2.jsx
@@ -0,0 +1,20 @@
+export default function Page2() {
+  return (
+    <div className="page2">
+      <div className="banner">
+        <div className="bannerimg"></div>
+        <div className="bannercontent">
+          <div className="bannerhead">
+            Streamline Your Billing Process with Ease!
+          </div>
+          <div className="bannerdesc">
+            Say goodbye to paper bills and complex processes. Generate, send,
+            and manage digital bills seamlessly with our Superbill platform.
+            Secure, efficient, and designed to keep your business and customers
+            connected.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
