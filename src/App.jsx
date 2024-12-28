d5c2417526754225286d91ef71b3da1b1b25b2c1
abhishekkjainn <jainabhishek1904@gmail.com>
2024-12-28 15:45:30 +0530

    Last page added in landing page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Page2 from './pages/page2';
 import Page3 from './pages/page3';
 import Page4 from './pages/page4';
 import Page5 from './pages/page5';
+import Page6 from './pages/page6';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
       <Page3 />
       <Page4 />
       <Page5 />
+      <Page6 />
+      <Footer />
     </>
   );
 }
