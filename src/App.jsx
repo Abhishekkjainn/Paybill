d1a1abe33b945dbce28f99a37e8f2eccbedde247
abhishekkjainn <jainabhishek1904@gmail.com>
2024-12-28 15:55:32 +0530

    Added Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header';
-import Page1 from './pages/page1';
-import Page2 from './pages/page2';
-import Page3 from './pages/page3';
-import Page4 from './pages/page4';
-import Page5 from './pages/page5';
 import Page6 from './pages/page6';
 import Footer from './components/footer';
+import Home from './homepage';
 
 function App() {
   return (
     <>
       <Header />
-      <Page1 />
-      <Page2 />
-      <Page3 />
-      <Page4 />
-      <Page5 />
+      <Home />
       <Page6 />
       <Footer />
     </>
---
+++ src/homepage.jsx
@@ -0,0 +1,17 @@
+import Page1 from './pages/page1';
+import Page2 from './pages/page2';
+import Page3 from './pages/page3';
+import Page4 from './pages/page4';
+import Page5 from './pages/page5';
+
+export default function Home() {
+  return (
+    <>
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
+    </>
+  );
+}
